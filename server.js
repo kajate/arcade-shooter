2fbf176809f2f3b303269d19ededeefe64a7dcfb
kajate <patrick.gardinger@gmail.com>
2018-06-29 13:13:22 +0200

    calibrating points system

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var players = {};
 //I will, in the means of time, fix all of this repetetiveteteive code.
 //Most probabably by creating a set of different attributs, in groups,
 //which I call on each property re-spawn, for now, this is a quick-fix.
-
+//But in any case, © Patrick Ebbmo Gårdinger 2018
 var scores = {
     blue: 0,
     red: 0
@@ -25,109 +25,99 @@ var star = {
 };
 
 var alien = {
-    x: Math.floor(Math.random() * 7000) + 6000,
-    y: Math.floor(Math.random() * 650) + 50,
-    vx: Math.floor(Math.random() * -500) + -100,
-    vy: Math.floor(Math.random() * -10) + 10,
-    r: Math.floor(Math.random() * 50) + -50,
-    angv: Math.floor(Math.random() * 145) + 115
+    x: Math.floor(Math.random() * 15000) + 10000,
+    y: Math.floor(Math.random() * 600) + 100,
+    vx: Math.floor(Math.random() * -1700) + -1000,
+    vy: Math.floor(Math.random() * 50) + -50,
+    r: Math.floor(Math.random() * 10) + -10,
+    angv: Math.floor(Math.random() * 50) + 10
 };
 
 var aubergine = {
-    x: Math.floor(Math.random() * 4000) + 3000,
+    x: Math.floor(Math.random() * 4000) + 3500,
     y: Math.floor(Math.random() * 650) + 50,
-    vx: Math.floor(Math.random() * -500) + -30,
-    vy: Math.floor(Math.random() * -10) + 10,
+    vx: Math.floor(Math.random() * -100) + -300,
+    vy: Math.floor(Math.random() * 50) + -50,
     r: Math.floor(Math.random() * 50) + -50,
-    angv: Math.floor(Math.random() * 145) + 115
+    angv: Math.floor(Math.random() * 150) + 50
 };
 
 var poop = {
-    x: Math.floor(Math.random() * 2500) + 2050,
-    y: Math.floor(Math.random() * 650) + 250,
-    vx: Math.floor(Math.random() * -80) + -40,
-    vy: Math.floor(Math.random() * -50) + 50,
+    x: Math.floor(Math.random() * 2700) + 2200,
+    y: Math.floor(Math.random() * 600) + 200,
+    vx: Math.floor(Math.random() * -100) + -40,
+    vy: Math.floor(Math.random() * 50) + -50,
     r: Math.floor(Math.random() * 50) + -50,
-    angv: Math.floor(Math.random() * 145) + 15
+    angv: Math.floor(Math.random() * 150) + 50
 };
 
 var tesla = {
-    x: Math.floor(Math.random() * 6200) + 6700,
-    y: Math.floor(Math.random() * 650) + 50,
-    vx: Math.floor(Math.random() * -150) + -100,
-    vy: Math.floor(Math.random() * -50) + 50,
+    x: Math.floor(Math.random() * 7000) + 6000,
+    y: Math.floor(Math.random() * 600) + 150,
+    vx: Math.floor(Math.random() * -400) + -100,
+    vy: Math.floor(Math.random() * -50) + -50,
     r: Math.floor(Math.random() * 50) + -50,
-    angv: Math.floor(Math.random() * 145) + 115
+    angv: Math.floor(Math.random() * 150) + 50
 };
 
 var pizza = {
-    x: Math.floor(Math.random() * 5000) + 5500,
+    x: Math.floor(Math.random() * 4000) + 3500,
     y: Math.floor(Math.random() * 650) + 50,
-    vx: Math.floor(Math.random() * -250) + -100,
-    vy: Math.floor(Math.random() * -5) + 5,
+    vx: Math.floor(Math.random() * -100) + -300,
+    vy: Math.floor(Math.random() * 50) + -50,
     r: Math.floor(Math.random() * 50) + -50,
-    angv: Math.floor(Math.random() * 145) + 115
+    angv: Math.floor(Math.random() * 150) + 50
 };
 
 var peach = {
-    x: Math.floor(Math.random() * 4000) + 3000,
+    x: Math.floor(Math.random() * 4000) + 3500,
     y: Math.floor(Math.random() * 650) + 50,
-    vx: Math.floor(Math.random() * -500) + -30,
-    vy: Math.floor(Math.random() * -50) + 50,
+    vx: Math.floor(Math.random() * -100) + -300,
+    vy: Math.floor(Math.random() * 50) + -50,
     r: Math.floor(Math.random() * 50) + -50,
-    angv: Math.floor(Math.random() * 145) + 115
+    angv: Math.floor(Math.random() * 150) + 50
 };
 
 var alien = {
     x: Math.floor(Math.random() * 4000) + 3000,
     y: Math.floor(Math.random() * 650) + 50,
-    vx: Math.floor(Math.random() * -500) + -30,
-    vy: Math.floor(Math.random() * -2) + 2,
+    vx: Math.floor(Math.random() * -1000) + -600,
+    vy: Math.floor(Math.random() * 50) + -50,
     r: Math.floor(Math.random() * 50) + -50,
-    angv: Math.floor(Math.random() * 145) + 115
-};
-
-var meteroidStart = {
-    y: Math.floor(Math.random() * 700) + 50,
-    dw: Math.floor(Math.random() * 1000) + 20,
-    dh: Math.floor(Math.random() * 1000) + 20,
-    vx: Math.floor(Math.random() * -350) + 50,
-    vy: Math.floor(Math.random() * -50) + 50,
-    r: Math.floor(Math.random() * -50) + 50,
-    angv: Math.floor(Math.random() * 50) + 10
+    angv: Math.floor(Math.random() * 150) + 50
 };
 
 var meteroid = {
-    x: Math.floor(Math.random() * 2000) + 1500,
+    x: Math.floor(Math.random() * 2500) + 2000,
     y: Math.floor(Math.random() * 700) + 50,
     dw: Math.floor(Math.random() * 1000) + 20,
-    dh: Math.floor(Math.random() * 1000) + 20,
-    vx: Math.floor(Math.random() * -350) + 50,
+    dh: Math.floor(Math.random() * 500) + 20,
+    vx: Math.floor(Math.random() * -350) + 100,
     vy: Math.floor(Math.random() * -50) + 50,
     r: Math.floor(Math.random() * -50) + 50,
-    angv: Math.floor(Math.random() * 50) + 10
+    angv: Math.floor(Math.random() * 150) + 50
 };
 
 var meteroidTwo = {
-    x: Math.floor(Math.random() * 2000) + 2500,
+    x: Math.floor(Math.random() * 2500) + 200,
     y: Math.floor(Math.random() * 700) + 50,
     dw: Math.floor(Math.random() * 1000) + 20,
-    dh: Math.floor(Math.random() * 1000) + 20,
-    vx: Math.floor(Math.random() * -350) + 50,
+    dh: Math.floor(Math.random() * 500) + 20,
+    vx: Math.floor(Math.random() * -550) + 50,
     vy: Math.floor(Math.random() * -50) + 50,
     r: Math.floor(Math.random() * -50) + 50,
-    angv: Math.floor(Math.random() * 50) + 10
+    angv: Math.floor(Math.random() * 150) + 50
 };
 
 var meteroidThree = {
-    x: Math.floor(Math.random() * 2500) + 3000,
+    x: Math.floor(Math.random() * 3000) + 2500,
     y: Math.floor(Math.random() * 700) + 50,
     dw: Math.floor(Math.random() * 1000) + 20,
-    dh: Math.floor(Math.random() * 1000) + 20,
+    dh: Math.floor(Math.random() * 500) + 20,
     vx: Math.floor(Math.random() * -350) + 50,
     vy: Math.floor(Math.random() * -50) + 50,
     r: Math.floor(Math.random() * -50) + 50,
-    angv: Math.floor(Math.random() * 50) + 10
+    angv: Math.floor(Math.random() * 150) + 50
 };
 
 app.use(express.static(__dirname + "/public"));
@@ -202,29 +192,61 @@ io.on("connection", function(socket) {
         io.emit("starLocation", star);
     });
 
+    socket.on("alienCollected", function() {
+        if (players[socket.id].team === "blue") {
+            scores.blue += Math.floor(Math.random() * 20000) + 1;
+        } else {
+            scores.red += Math.floor(Math.random() * 20000) + 1;
+        }
+        alien.x = Math.floor(Math.random() * 15000) + 10000;
+        alien.y = Math.floor(Math.random() * 600) + 100;
+        alien.vx = Math.floor(Math.random() * -1700) + -1000;
+        alien.vy = Math.floor(Math.random() * 50) + -50;
+        alien.r = Math.floor(Math.random() * 10) + -10;
+        alien.angv = Math.floor(Math.random() * 50) + 10;
+        io.emit("alienData", alien);
+        io.emit("scoreUpdate", scores);
+    });
+
+    socket.on("alienMissedAndReset", function() {
+        if (players[socket.id].team === "blue") {
+            scores.blue -= Math.floor(Math.random() * 15000) + 5000;
+        } else {
+            scores.red -= Math.floor(Math.random() * 15000) + 5000;
+        }
+        alien.x = Math.floor(Math.random() * 15000) + 10000;
+        alien.y = Math.floor(Math.random() * 600) + 100;
+        alien.vx = Math.floor(Math.random() * -1700) + -1000;
+        alien.vy = Math.floor(Math.random() * 50) + -50;
+        alien.r = Math.floor(Math.random() * 10) + -10;
+        alien.angv = Math.floor(Math.random() * 50) + 10;
+        io.emit("alienData", alien);
+        io.emit("scoreUpdate", scores);
+    });
+
     socket.on("aubergineCollected", function() {
         if (players[socket.id].team === "blue") {
             scores.blue += 500;
         } else {
             scores.red += 500;
         }
-        aubergine.x = Math.floor(Math.random() * 3500) + 3000;
+        aubergine.x = Math.floor(Math.random() * 4000) + 3500;
         aubergine.y = Math.floor(Math.random() * 650) + 50;
-        aubergine.r = Math.floor(Math.random() * 50) + -50;
-        aubergine.vx = Math.floor(Math.random() * -100) + -200;
+        aubergine.vx = Math.floor(Math.random() * -100) + -300;
         aubergine.vy = Math.floor(Math.random() * 50) + -50;
-        aubergine.angv = Math.floor(Math.random() * 145) + 115;
+        aubergine.r = Math.floor(Math.random() * 50) + -50;
+        aubergine.angv = Math.floor(Math.random() * 150) + 50;
         io.emit("aubergineData", aubergine);
         io.emit("scoreUpdate", scores);
     });
 
     socket.on("aubergineMissedAndReset", function() {
-        aubergine.r = Math.floor(Math.random() * 50) + -50;
-        aubergine.vx = Math.floor(Math.random() * -200) + -100;
-        aubergine.vy = Math.floor(Math.random() * -50) + 50;
-        aubergine.x = Math.floor(Math.random() * 2200) + 2550;
+        aubergine.x = Math.floor(Math.random() * 4000) + 3500;
         aubergine.y = Math.floor(Math.random() * 650) + 50;
-        aubergine.angv = Math.floor(Math.random() * 145) + 115;
+        aubergine.vx = Math.floor(Math.random() * -100) + -300;
+        aubergine.vy = Math.floor(Math.random() * 50) + -50;
+        aubergine.r = Math.floor(Math.random() * 50) + -50;
+        aubergine.angv = Math.floor(Math.random() * 150) + 50;
         io.emit("aubergineData", aubergine);
     });
 
@@ -260,49 +282,49 @@ io.on("connection", function(socket) {
         } else {
             scores.red += 10000;
         }
+        tesla.x = Math.floor(Math.random() * 7000) + 6000;
+        tesla.y = Math.floor(Math.random() * 600) + 150;
+        tesla.vx = Math.floor(Math.random() * -400) + -100;
+        tesla.vy = Math.floor(Math.random() * -50) + -50;
         tesla.r = Math.floor(Math.random() * 50) + -50;
-        tesla.vx = Math.floor(Math.random() * -200) + -100;
-        tesla.vy = Math.floor(Math.random() * -20) + 20;
-        tesla.x = Math.floor(Math.random() * 6200) + 6550;
-        tesla.y = Math.floor(Math.random() * 650) + 50;
-        tesla.angv = Math.floor(Math.random() * 145) + 115;
+        tesla.angv = Math.floor(Math.random() * 150) + 50;
         io.emit("teslaData", tesla);
         io.emit("scoreUpdate", scores);
     });
 
     socket.on("teslaMissedAndReset", function() {
+        tesla.x = Math.floor(Math.random() * 7000) + 6000;
+        tesla.y = Math.floor(Math.random() * 600) + 150;
+        tesla.vx = Math.floor(Math.random() * -400) + -100;
+        tesla.vy = Math.floor(Math.random() * -50) + -50;
         tesla.r = Math.floor(Math.random() * 50) + -50;
-        tesla.vx = Math.floor(Math.random() * -200) + -100;
-        tesla.vy = Math.floor(Math.random() * -20) + 20;
-        tesla.x = Math.floor(Math.random() * 6200) + 6550;
-        tesla.y = Math.floor(Math.random() * 650) + 50;
-        tesla.angv = Math.floor(Math.random() * 145) + 115;
+        tesla.angv = Math.floor(Math.random() * 150) + 50;
         io.emit("teslaData", tesla);
     });
 
     socket.on("pizzaCollected", function() {
         if (players[socket.id].team === "blue") {
-            scores.blue += 5000;
+            scores.blue += 6969;
         } else {
-            scores.red += 5000;
+            scores.red += 6969;
         }
-        pizza.vx = Math.floor(Math.random() * -250) + -100;
-        pizza.vy = Math.floor(Math.random() * -20) + 20;
-        pizza.x = Math.floor(Math.random() * 6200) + 6550;
+        pizza.x = Math.floor(Math.random() * 4000) + 3500;
         pizza.y = Math.floor(Math.random() * 650) + 50;
+        pizza.vx = Math.floor(Math.random() * -100) + -300;
+        pizza.vy = Math.floor(Math.random() * 50) + -50;
         pizza.r = Math.floor(Math.random() * 50) + -50;
-        pizza.angv = Math.floor(Math.random() * 145) + 115;
+        pizza.angv = Math.floor(Math.random() * 150) + 50;
         io.emit("pizzaData", pizza);
         io.emit("scoreUpdate", scores);
     });
 
     socket.on("pizzaMissedAndReset", function() {
-        pizza.r = Math.floor(Math.random() * 50) + -50;
-        pizza.vx = Math.floor(Math.random() * -200) + -100;
-        pizza.vy = Math.floor(Math.random() * -20) + 20;
-        pizza.x = Math.floor(Math.random() * 6200) + 6550;
+        pizza.x = Math.floor(Math.random() * 4000) + 3500;
         pizza.y = Math.floor(Math.random() * 650) + 50;
-        pizza.angv = Math.floor(Math.random() * 145) + 115;
+        pizza.vx = Math.floor(Math.random() * -100) + -300;
+        pizza.vy = Math.floor(Math.random() * 50) + -50;
+        pizza.r = Math.floor(Math.random() * 50) + -50;
+        pizza.angv = Math.floor(Math.random() * 150) + 50;
         io.emit("pizzaData", pizza);
     });
 
@@ -312,23 +334,23 @@ io.on("connection", function(socket) {
         } else {
             scores.red += 500;
         }
-        peach.r = Math.floor(Math.random() * 50) + -50;
-        peach.vx = Math.floor(Math.random() * -200) + -100;
-        peach.vy = Math.floor(Math.random() * -50) + 50;
-        peach.x = Math.floor(Math.random() * 2200) + 2550;
+        peach.x = Math.floor(Math.random() * 4000) + 3500;
         peach.y = Math.floor(Math.random() * 650) + 50;
-        peach.angv = Math.floor(Math.random() * 145) + 115;
+        peach.vx = Math.floor(Math.random() * -100) + -300;
+        peach.vy = Math.floor(Math.random() * 50) + -50;
+        peach.r = Math.floor(Math.random() * 50) + -50;
+        peach.angv = Math.floor(Math.random() * 150) + 50;
         io.emit("peachData", peach);
         io.emit("scoreUpdate", scores);
     });
 
     socket.on("peachMissedAndReset", function() {
-        peach.r = Math.floor(Math.random() * 50) + -50;
-        peach.vx = Math.floor(Math.random() * -200) + -100;
-        peach.vy = Math.floor(Math.random() * -50) + 50;
-        peach.x = Math.floor(Math.random() * 2200) + 2550;
+        peach.x = Math.floor(Math.random() * 4000) + 3500;
         peach.y = Math.floor(Math.random() * 650) + 50;
-        peach.angv = Math.floor(Math.random() * 145) + 115;
+        peach.vx = Math.floor(Math.random() * -100) + -300;
+        peach.vy = Math.floor(Math.random() * 50) + -50;
+        peach.r = Math.floor(Math.random() * 50) + -50;
+        peach.angv = Math.floor(Math.random() * 150) + 50;
         io.emit("peachData", peach);
     });
 
